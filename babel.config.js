cd54f1c348bbe6c5127bcd9c5b3a34440b7b5b1b
carloshdt <carloshdt@msn.com>
2026-04-29 23:55:44 -0300

    fix: remover reanimated plugin do babel (não utilizado ainda)

--- babel.config.js
+++ babel.config.js
@@ -1,13 +1,11 @@
 module.exports = function (api) {
   api.cache(true);
-  const isTest = process.env.NODE_ENV === 'test';
   return {
     presets: [
       [
         require('./node_modules/expo/node_modules/babel-preset-expo'),
-        { jsxImportSource: 'nativewind', reanimated: !isTest },
+        { jsxImportSource: 'nativewind', reanimated: false },
       ],
     ],
-    plugins: isTest ? [] : ['react-native-reanimated/plugin'],
   };
 };
